7522958cfccc86137dd74d264171ddaf13a4efc4
alexclp <alexandru.clapa@gmail.com>
2015-09-01 18:12:44 +0300

    Netoworking class

---
+++ HomeBuyerAlert/Networking.h
@@ -0,0 +1,13 @@
+//
+//  Networking.h
+//  HomeBuyerAlert
+//
+//  Created by Alexandru Clapa on 01.09.2015.
+//  Copyright (c) 2015 Alexandru Clapa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Networking : NSObject
+
+@end
---
+++ HomeBuyerAlert/Networking.m
@@ -0,0 +1,13 @@
+//
+//  Networking.m
+//  HomeBuyerAlert
+//
+//  Created by Alexandru Clapa on 01.09.2015.
+//  Copyright (c) 2015 Alexandru Clapa. All rights reserved.
+//
+
+#import "Networking.h"
+
+@implementation Networking
+
+@end
