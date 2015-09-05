ac1a66fc505238b7d5ded09866b03a1db42a1039
alexclp <alexandru.clapa@gmail.com>
2015-09-05 17:50:06 +0300

    Cell class

---
+++ HomeBuyerAlert/ListViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ListViewCell.h
+//  HomeBuyerAlert
+//
+//  Created by Alexandru Clapa on 05.09.2015.
+//  Copyright (c) 2015 Alexandru Clapa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListViewCell : UITableViewCell
+
+@end
