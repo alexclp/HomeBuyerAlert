595bbdb0b315e11b99adc2bb8cc66a4bcba51c05
alexclp <alexandru.clapa@gmail.com>
2015-09-09 19:40:14 +0300

    Map stuff done

--- HomeBuyerAlert/MapViewController.m
+++ HomeBuyerAlert/MapViewController.m
@@ -17,6 +17,8 @@
 @interface MapViewController ()
 
 @property (nonatomic, strong) NSArray *coordinates;
+@property (nonatomic, assign) NSInteger index;
+@property (nonatomic, strong) NSArray *properties;
 
 @end
 
@@ -33,25 +35,24 @@
 		if (error) {
 			
 		} else {
-			NSLog(@"No error");
 			
 			NSMutableArray *temp = [NSMutableArray array];
 			
-			for (Property *property in array) {
+			self.properties = array;
+			
+			for (Property *property in self.properties) {
 				NSString *latitude = property.latitude;
 				NSString *longitude = property.longitude;
 				NSString *title = property.title;
-				
-				NSLog(@"Latitude = %@", latitude);
-				NSLog(@"Longitude = %@", longitude);
-				NSLog(@"Title = %@", title);
 				
 				[temp addObject:@{@"latitude": latitude, @"longitude": longitude, @"title": title}];
 			}
 			
 			self.coordinates = temp.copy;
 			
+			self.index = 0;
 			[self.mapView addAnnotations:[self configureAnnotations]];
+			[self zoomToLocation];
 			
 		}
 	}];
@@ -74,12 +75,14 @@
 	return annotations;
 }
 
-- (MKAnnotationView *)mapView:(MKMapView *)mV viewForAnnotation:(id )annotation
+- (MKAnnotationView *)mapView:(MKMapView *)mV viewForAnnotation:(id)annotation
 {
+	Property *current = [self.properties objectAtIndex:self.index];
 	UIView *pinView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"pin.png"]];
+	pinView.frame = CGRectMake(-5, 0, 34, 34);
 	AnnotationView *calloutView = [[[NSBundle mainBundle] loadNibNamed:@"AnnotationView" owner:self options:nil] firstObject];
-	calloutView.title.text = @"Title";
-	calloutView.image.image = [UIImage imageNamed:@"placeholder.png"];
+	calloutView.title.text = current.title;
+	calloutView.image.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:current.imageURL]]];
 	
 	DXAnnotationView *annotationView = (DXAnnotationView *)[mV dequeueReusableAnnotationViewWithIdentifier:NSStringFromClass([DXAnnotationView class])];
 	if (!annotationView) {
@@ -89,7 +92,22 @@
 														  calloutView:calloutView
 															 settings:[DXAnnotationSettings defaultSettings]];
 	}
+	
+	self.index ++;
+	
 	return annotationView;
+}
+
+- (void)zoomToLocation {
+	NSDictionary *firstLocation = [self.coordinates objectAtIndex:0];
+	MKCoordinateRegion region;
+	region.center.latitude = [[firstLocation objectForKey:@"latitude"] doubleValue];
+	region.center.longitude = [[firstLocation objectForKey:@"longitude"] doubleValue];
+	
+	region.span.latitudeDelta = 30.0;
+	region.span.longitudeDelta = 30.0;
+	
+	[self.mapView setRegion:region animated:YES];
 }
 
 @end
